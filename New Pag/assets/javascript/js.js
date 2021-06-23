9b67b96609d19a863453760fd226eb0a915e4ebd
Gonzales Walisson <walisson.gonzales@gmail.com>
2021-06-23 03:32:14 -0300

    Versão da pagina Boku no Hero

--- New Pag/assets/javascript/js.js
+++
@@ -1,7 +0,0 @@
-(function () {
-    let menu = document.getElementById('navbar'); // colocar em cache
-    window.addEventListener('scroll', function () {
-        if (window.scrollY > 0) menu.classList.add('menuFixo'); // > 0 ou outro valor desejado
-        else menu.classList.remove('menuFixo');
-    });
-})();
